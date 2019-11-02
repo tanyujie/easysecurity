f30c6bcc54ae404a3cf0a9d9929aeea591f2dc08
Administrator <Administrator@SC-201907201119>
2019-11-02 19:36:52 +0800

    init

---
+++ src/main/java/org/easymis/easysecurity/service/CompanyOtherService.java
@@ -0,0 +1,7 @@
+package org.easymis.easysecurity.service;
+
+import org.easymis.easysecurity.entitys.mybatis.dto.CompanyOther;
+
+public interface CompanyOtherService {
+	public CompanyOther getById(String companyId);
+}
---
+++ src/main/java/org/easymis/easysecurity/service/impl/CompanyOtherServiceImpl.java
@@ -0,0 +1,14 @@
+package org.easymis.easysecurity.service.impl;
+
+import org.easymis.easysecurity.entitys.mybatis.dto.CompanyOther;
+import org.easymis.easysecurity.service.CompanyOtherService;
+
+public class CompanyOtherServiceImpl implements CompanyOtherService {
+
+	@Override
+	public CompanyOther getById(String companyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

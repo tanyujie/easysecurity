88f98ff340de17cc27c201cd9e583b19fb0dd3c4
Administrator <Administrator@SC-201907201119>
2019-11-02 20:53:23 +0800

    init

--- src/main/java/org/easymis/easysecurity/config/WebSocketConfig.java
+++ src/main/java/org/easymis/easysecurity/config/WebSocketConfig.java
@@ -1,14 +1,13 @@
 package org.easymis.easysecurity.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 /**
  * 
  * @author 谭宇杰
  *
  */
-@Component
+/*@Component*/
 public class WebSocketConfig {
 	@Bean
     public ServerEndpointExporter serverEndpointExporter() {

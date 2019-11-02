774b94074aa3a8aa3a6ff78c0293da861e598035
Administrator <Administrator@SC-201907201119>
2019-11-02 20:23:14 +0800

    init

--- src/main/java/org/easymis/easysecurity/service/impl/CompanyOtherServiceImpl.java
+++ src/main/java/org/easymis/easysecurity/service/impl/CompanyOtherServiceImpl.java
@@ -2,7 +2,8 @@ package org.easymis.easysecurity.service.impl;
 
 import org.easymis.easysecurity.entitys.mybatis.dto.CompanyOther;
 import org.easymis.easysecurity.service.CompanyOtherService;
-
+import org.springframework.stereotype.Service;
+@Service
 public class CompanyOtherServiceImpl implements CompanyOtherService {
 
 	@Override
